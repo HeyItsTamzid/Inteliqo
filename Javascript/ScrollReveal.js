17e962414389f9b3b0ee7b05b11c346f9164c908
TAMZID HASSAN <iamtamzid96@gmail.com>
2020-02-29 00:10:03 +0600

    Initial Template
    Initial Template Done

---
+++ Javascript/ScrollReveal.js
@@ -0,0 +1,18 @@
+//  Scroll Reveal Example
+// ScrollReveal().reveal('#typingEffect', {
+//   origin: 'top',
+//   duration: 1000,
+//   distance: '150px',
+//   reset: true
+// });
+
+// ScrollReveal().reveal('#mobo', {
+//   origin: 'top',
+//   duration: 1000,
+//   distance: '200px',
+//   rotate: {
+//     x: 5,
+//     y: 50,
+//     z: 100
+//   }
+// });
---
+++ Javascript/SmoothScroll.js
@@ -0,0 +1,8 @@
+// SmoothScrolling Example
+// $("#showcaseBtn").click(function () {
+
+//   var jumpTo = $("#what-we-do");
+//   var new_position = $(jumpTo).offset();
+//   $('html, body').stop().animate({ scrollTop: new_position.top }, 1000);
+//   event.preventDefault();
+// });
