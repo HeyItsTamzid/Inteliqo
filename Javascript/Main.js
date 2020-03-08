9c84c584e98ab1815c6d83efe3bf6931c834a3be
TAMZID HASSAN <iamtamzid96@gmail.com>
2020-03-09 02:19:42 +0600

    checking if forms works

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -4,110 +4,133 @@
 //   speed: 700,
 //   speedAsDuration: true,
 //   clip: true,
-//   offset: 10 
+//   offset: 10
 // });
 
-const hamburger = document.querySelector('.hamburger');
-const navList = document.querySelector('.navigation-list');
-const navLinks = document.querySelectorAll('.nav-links');
-const overlay = document.querySelector('#overlay');
-const socialLinks = document.querySelector('#social-links');
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".navigation-list");
+const navLinks = document.querySelectorAll(".nav-links");
+const overlay = document.querySelector("#overlay");
+const socialLinks = document.querySelector("#social-links");
 let opened = false;
 
 function addActive(element) {
-  element.classList.add('active');
+  element.classList.add("active");
 }
 function removeActive(element) {
-  element.classList.remove('active');
+  element.classList.remove("active");
 }
 
-hamburger.addEventListener('click', () => {
-  if(!opened) {
+hamburger.addEventListener("click", () => {
+  if (!opened) {
     addActive(hamburger);
     addActive(navList);
     addActive(socialLinks);
-    for(let i = 0; i < navLinks.length; i++) {
+    for (let i = 0; i < navLinks.length; i++) {
       addActive(navLinks[i]);
-     }
-    overlay.classList.add('nav-is-active');
-    overlay.style.zIndex = '1';
-    opened = true;
-   } else {
-      removeActive(hamburger);
-      removeActive(navList);
-      removeActive(socialLinks);
-      for(let i = 0; i < navLinks.length; i++) {
-        removeActive(navLinks[i]);
-      }
-      overlay.classList.remove('nav-is-active');
-      overlay.style.zIndex = '-1';
-      navList.classList.add('close-nav');
-      opened = false;
     }
-});
-
-navList.addEventListener('click', (event) => {
-  if(event.target.tagName === 'A') {
+    overlay.classList.add("nav-is-active");
+    overlay.style.zIndex = "1";
+    opened = true;
+  } else {
     removeActive(hamburger);
     removeActive(navList);
     removeActive(socialLinks);
-    overlay.classList.remove('nav-is-active');
-    for(let i = 0; i < navLinks.length; i++) {
+    for (let i = 0; i < navLinks.length; i++) {
       removeActive(navLinks[i]);
     }
-    overlay.style.zIndex = '-1';
-    navList.classList.add('close-nav');
+    overlay.classList.remove("nav-is-active");
+    overlay.style.zIndex = "-1";
+    navList.classList.add("close-nav");
+    opened = false;
+  }
+});
+
+navList.addEventListener("click", event => {
+  if (event.target.tagName === "A") {
+    removeActive(hamburger);
+    removeActive(navList);
+    removeActive(socialLinks);
+    overlay.classList.remove("nav-is-active");
+    for (let i = 0; i < navLinks.length; i++) {
+      removeActive(navLinks[i]);
+    }
+    overlay.style.zIndex = "-1";
+    navList.classList.add("close-nav");
   }
 });
 // NAVBAR ENDS
 
-
 // Contact us form starts
-const
-placeholders = document.querySelectorAll('.styled-input__placeholder-text'),
-inputs = document.querySelectorAll('.styled-input__input');
+const placeholders = document.querySelectorAll(
+    ".styled-input__placeholder-text"
+  ),
+  inputs = document.querySelectorAll(".styled-input__input");
 
-placeholders.forEach(function (el, i) {
+placeholders.forEach(function(el, i) {
   let value = el.innerText,
-  html = '';
+    html = "";
   for (let w of value) {
-    if (!value) value = '&nbsp;';
+    if (!value) value = "&nbsp;";
     html += `<span class="letter">${w}</span>`;
   }
   el.innerHTML = html;
 });
 
-inputs.forEach(function (el) {
+inputs.forEach(function(el) {
   let parent = el.parentNode;
-  el.addEventListener('focus', function () {
-    parent.classList.add('filled');
-    placeholderAnimationIn(parent, true);
-  }, false);
-  el.addEventListener('blur', function () {
-    if (el.value.length) return;
-    parent.classList.remove('filled');
-    placeholderAnimationIn(parent, false);
-  }, false);
+  el.addEventListener(
+    "focus",
+    function() {
+      parent.classList.add("filled");
+      placeholderAnimationIn(parent, true);
+    },
+    false
+  );
+  el.addEventListener(
+    "blur",
+    function() {
+      if (el.value.length) return;
+      parent.classList.remove("filled");
+      placeholderAnimationIn(parent, false);
+    },
+    false
+  );
 });
 
 function placeholderAnimationIn(parent, action) {
-  let act = action ? 'add' : 'remove';
-  let letters = parent.querySelectorAll('.letter');
+  let act = action ? "add" : "remove";
+  let letters = parent.querySelectorAll(".letter");
   letters = [].slice.call(letters, 0);
   if (!action) letters = letters.reverse();
-  letters.forEach(function (el, i) {
-    setTimeout(function () {
-      let contains = parent.classList.contains('filled');
-      if (action && !contains || !action && contains) return;
-      el.classList[act]('active');
+  letters.forEach(function(el, i) {
+    setTimeout(function() {
+      let contains = parent.classList.contains("filled");
+      if ((action && !contains) || (!action && contains)) return;
+      el.classList[act]("active");
     }, 50 * i);
   });
 }
 
-setTimeout(function () {
-  document.body.classList.add('on-start');
+setTimeout(function() {
+  document.body.classList.add("on-start");
 }, 100);
 
-setTimeout(function () {
-  document.body.classList.add('document-loaded');
+setTimeout(function() {
+  document.body.classList.add("document-loaded");
 }, 1800);
+
+// handling form submit
+$("contactForm").submit(function(e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    Swal.fire({
+      title: "Success",
+      text: "We will soon contact you.",
+      type: "success",
+      confirmButtonText: "Cool"
+    });
+  });
+});
