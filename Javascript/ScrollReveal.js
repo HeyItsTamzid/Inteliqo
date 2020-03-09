e3c4d12370d005be355edefbcab7af622e6778f5
TAMZID HASSAN <iamtamzid96@gmail.com>
2020-03-09 19:54:01 +0600

    animations added

--- Javascript/ScrollReveal.js
+++ Javascript/ScrollReveal.js
@@ -1,18 +1,71 @@
 //  Scroll Reveal Example
-// ScrollReveal().reveal('#typingEffect', {
-//   origin: 'top',
-//   duration: 1000,
-//   distance: '150px',
-//   reset: true
-// });
+ScrollReveal().reveal("#logo", {
+  origin: "bottom",
+  duration: 1500,
+  distance: "50px",
+  reset: false
+});
 
-// ScrollReveal().reveal('#mobo', {
-//   origin: 'top',
-//   duration: 1000,
-//   distance: '200px',
-//   rotate: {
-//     x: 5,
-//     y: 50,
-//     z: 100
-//   }
-// });
+ScrollReveal().reveal("nav", {
+  origin: "center",
+  duration: 1500,
+  distance: "50px",
+  rotate: {
+    x: 0,
+    y: 0,
+    z: 250
+  }
+});
+
+ScrollReveal().reveal("#heroSubTitle", {
+  origin: "bottom",
+  delay: 500,
+  duration: 1500,
+  distance: "50px",
+  reset: false
+});
+
+ScrollReveal().reveal("#heroBtnSmall", {
+  origin: "left",
+  delay: 800,
+  duration: 1500,
+  distance: "150px",
+  reset: false
+});
+
+ScrollReveal().reveal(".learn-more", {
+  origin: "left",
+  delay: 800,
+  duration: 1500,
+  distance: "150px",
+  reset: false
+});
+
+ScrollReveal().reveal(".sectionTitle", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "50px",
+  reset: false
+});
+
+ScrollReveal().reveal(".sectionSubTitle", {
+  origin: "bottom",
+  duration: 1000,
+  delay: 500,
+  distance: "50px",
+  reset: false
+});
+
+ScrollReveal().reveal(".leftItem", {
+  origin: "left",
+  duration: 1500,
+  distance: "150px",
+  reset: false
+});
+
+ScrollReveal().reveal(".rightItem", {
+  origin: "right",
+  duration: 1500,
+  distance: "150px",
+  reset: false
+});
